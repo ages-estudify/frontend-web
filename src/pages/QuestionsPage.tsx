87ff5a699c4207859a7e8d90659721e21d6746e2
Isadora Guerra <isa.f.guerra@hotmail.com>
2026-04-02 09:07:09 -0300

    feat: sidebar without style

---
+++ src/pages/QuestionsPage.tsx
@@ -0,0 +1,7 @@
+export function QuestionsPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-semibold text-gray-900">Questões</h1>
+    </div>
+  )
+}
