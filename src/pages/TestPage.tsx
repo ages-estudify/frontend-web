1a0acc5b9cfecd56622ae7e3c964781926a98a9a
Gabriel Saldanha de Negre <130391676+gbn0@users.noreply.github.com>
2026-04-04 20:54:19 -0300

    Revert "Feature(07.3)/sidebar (#3)" (#5)
    This reverts commit 3b9e8df61f91ea345260a8244ac5e1cfd6f98964.

--- src/pages/TestPage.tsx
+++
@@ -1,7 +0,0 @@
-export function TrackPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-semibold text-gray-900">Trilha</h1>
-    </div>
-  )
-}
